8222e50cf022cc2676c5651c1c4e1a749cb4cdf1
ericmuniiz <107154288+ericmuniiz@users.noreply.github.com>
2023-09-25 21:10:00 -0300

    Add files via upload

--- videos.js
+++ videos.js
@@ -1,37 +1,8 @@
-const videos = require("../models/videos");
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/videos");
 
-const database = require("../config/database");
+router.get('/videos', controller.showVideo);
+router.get('/video/:id', controller.showVideoById);
 
-
-// FUNÇÃO PARA VER AS VIDEOS
-exports.showVideo = (req, res) => {
-    const query = "SELECT * FROM VIDEOS";
-
-    database.query(query).then(
-        (resultado) => {
-            res.status(200).send({ mensagem: resultado.rows })
-        },
-        (erro) => {
-            res.status(500).send({ erro: erro })
-        }
-
-        
-    )
-}
-
-// FUNÇÃO PARA VER OS VIDEOS
-exports.showVideoById = (req, res) => {
-    const query = "SELECT * FROM VIDEOS WHERE id=$1;"
-    const values = [req.params.id];
-
-    database.query(query, values).then(
-        (resultado) => {
-            res.status(200).send({ mensagem: resultado.rows })
-        },
-        (erro) => {
-            res.status(500).send({ erro: erro })
-        }
-    )
-}
-
-
+module.exports = router;
